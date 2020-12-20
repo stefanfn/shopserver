b08e62f04fb08f3ddfb351f2e349b3d1175cf5d8
Stefan Sassenberg <stefan.sassenberg@gmx.de>
2020-12-20 11:07:58 +0100

    verwendet den verzeichnisnamen als namen fuer den container

--- launch.sh
+++ launch.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
+CONTAINER_NAME="$(basename $(pwd))"
+
 docker run -it --rm \
+  --name $CONTAINER_NAME \
   -p 54783:54783 \
   --mount type=bind,source="$(pwd)"/resources,target=/resources \
   shopserver ./start.sh
